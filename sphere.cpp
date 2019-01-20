0bdb1ae5653a136a33333e6889937b96b582f127
borninla <alvov001@ucr.edu>
2019-01-19 20:00:36 -0800

    Implemented computing normal vector of sphere

--- sphere.cpp
+++ sphere.cpp
@@ -11,7 +11,7 @@ Hit Sphere::Intersection(const Ray& ray, int part) const
 vec3 Sphere::Normal(const vec3& point, int part) const
 {
     vec3 normal;
-    TODO; // compute the normal direction
+    normal = 2 * (point - center);
     return normal;
 }
 
