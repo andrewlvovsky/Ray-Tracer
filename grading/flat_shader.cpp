4fbfb382434a46694d45e7c139dd3daa58154518
borninla <alvov001@ucr.edu>
2019-01-26 20:12:26 -0800

    .gitignore fix

--- grading/flat_shader.cpp
+++
@@ -1,8 +0,0 @@
-#include "flat_shader.h"
-
-vec3 Flat_Shader::
-Shade_Surface(const Ray& ray,const vec3& intersection_point,
-    const vec3& normal,int recursion_depth) const
-{
-    return color;
-}
