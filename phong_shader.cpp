b25734bc8ac2ece9189b53a5cd8f4352a1b0fd82
borninla <alvov001@ucr.edu>
2019-01-26 20:11:05 -0800

    implemented ambient shading, first checkpoint complete

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -9,6 +9,9 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
     vec3 color;
-    TODO; //determine the color
+    
+    // Currently, only ambient shading is implemented
+    color = color_ambient * world.ambient_color * world.ambient_intensity;
+
     return color;
 }
