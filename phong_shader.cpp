19889079b928955108f707ab6ff87c6e9a7315b8
Andrew Lvovsky <alvov001@ucr.edu>
2019-02-03 00:28:01 -0800

    implemented diffuse and specular shading, finished checkpoint 2

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -10,8 +10,28 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
     
-    // Currently, only ambient shading is implemented
-    color = color_ambient * world.ambient_color * world.ambient_intensity;
+    // I = (L_a * R_a) + (L_d * R_d * max(n*l, 0)) + (L_s * R_s * max(v*r, 0))
+
+    // Ambient calculation
+    vec3 ambient = world.ambient_color * world.ambient_intensity * this->color_ambient;
+
+    color = ambient;
+
+    // Adding diffuse and specular calculation to ambient
+    vec3 diffuse, specular;
+
+    for (auto light : world.lights) {
+        vec3 l = light->position - intersection_point;  // light ray
+        vec3 r = -l + 2 * dot(l, normal) * normal;  //reflection ray
+
+        diffuse = this->color_diffuse * light->Emitted_Light(l);
+        diffuse *= fmax(dot(normal, l.normalized()), 0.0);
+        color += diffuse;
+
+        specular = this->color_specular * light->Emitted_Light(l);
+        specular *= pow(fmax(dot(-ray.direction, r.normalized()), 0.0), specular_power);
+        color += specular;
+    }
 
     return color;
 }
